0787d96729d6ee8bd3c9641acedd06a83abc76e2
sanketpatelcoding <androsanket@gmail.com>
2025-08-08 09:01:42 +0530

    (7thAug)

--- panda-learn/panda4.py
+++ panda-learn/panda4.py
@@ -13,8 +13,11 @@ todo_dataframe = pd.DataFrame(todo_data)
 
 print("Displaying three columns (id, todo, completed):")
 print(todo_dataframe[['id','userId', 'todo', 'completed']])
+print("Displaying========order change==")
 print(todo_dataframe[['id','userId', 'todo']])
+print("Displaying=======few columns===")
 print(todo_dataframe[['id','userId', 'completed']])
+print("Displaying==========")
 # -----------------------------------------
 print("Displaying==========")
 print(todo_dataframe.info())
@@ -26,4 +29,10 @@ data = {'Name': ['Aman', 'Ankit', 'Vaibhav'],
         'Age': [25, 30, 35]
         }
 df = pd.DataFrame(data)
+print("Displaying========order change==")
 print(df[['Age','Name']])
+
+print("other way of arranging==")
+data1=[[23,'aman'],[30,'ankit'],[35,'vaibhavv']]
+df=pd.DataFrame(data1)
+print(df)
