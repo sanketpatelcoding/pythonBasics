e3499e3c43fdf7ea26b1a64f117aa6af1a7db76f
sanketpatelcoding <androsanket@gmail.com>
2025-08-19 09:00:15 +0530

    phonelearn started

---
+++ panda-learn/phonelearn.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+
+# def printheadtails(headcount, tailcount, df):
+#     print(f"first {headcount} rows df")
+#     print(df.head(headcount))
+#     print(f"last {tailcount} row")
+#     print(df.tail(tailcount))
+
+df = pd.read_csv("phone_data.csv")
+# head_rows = int(input("Enter number for head"))
+# tail_rows = int(input("Enter number for tail"))
+# printheadtails(head_rows, tail_rows, df)
+
+print('---------------------------------------------')
+# ----------------------------------------------------------
+# print("\ndataset info")
+# print(df.info())
+print('---------------------------------------------')
+# print("\ndataset dscription")
+# print(df.describe())
+print('---------------------------------------------')
+# ----------------------unique------------------------------
+print(df.head(25))
+# print(df.nunique())
+print('-------------- total number of calls-------')
+totalcalls = len(df[df['item'] == 'call'])
+print(f"\nTotal number of calls: {totalcalls}")
+print('-------------- Distinct values in the item column (self)-------------------------------')
+print("unoque values in the item column")
+print(df['item'].unique())
+print('-------------- total number of data incident-')
+totaldata=len(df[df['item']=='data'])
+print(f'{totaldata}  instances used in df')
+print('-------------- total number of sms-')
+totalsmscount=len(df[df['item']=='sms'])
+print(f' is totalsmscount is {totalsmscount}\n')
+
+# Calculate the total duration of all calls combined.
+durationtotal = df[df['item'] == 'call']['duration'].sum()
+print(f' is durationtotal is {durationtotal}')
+# Calculate the total duration of all calls combined.USEING NUMPY
+npdurationtotal=   np.sum(df[df['item'] == 'call']['duration'])
+print(f'USEING NUMPY is npdurationtotal is {npdurationtotal}')
+
