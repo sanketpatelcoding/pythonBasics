1ea9f9588650cd8b024ee302d363287e3f89623b
sanketpatelcoding <androsanket@gmail.com>
2025-09-25 08:34:11 +0530

    matplotlib1

--- matplotliblearn/mpl1.py
+++ matplotliblearn/mpl1.py
@@ -45,13 +45,81 @@ x = np.array([1, 2, 3, 4, 5])
 # plt.show()
 
 # 4scatter
-x = [1, 2, 3, 4, 5]
-y = [2, 4, 5, 4, 5]
-plt.figure(figsize=(6, 4))
-plt.scatter(x, y, color='purple', s=10)
-# s is scaller to cnortol size of marked point
-plt.title('Scatter Plot')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.grid(True)
+# x = [1, 2, 3, 4, 5]
+# y = [2, 4, 5, 4, 5]
+# plt.figure(figsize=(6, 4))
+# plt.scatter(x, y, color='purple', s=10)
+# # s is scaller to cnortol size of marked point
+# plt.title('Scatter Plot')
+# plt.xlabel('X-axis')
+# plt.ylabel('Y-axis')
+# plt.grid(True)
+# plt.show()
+
+
+# --------------------------------------------------------
+
+# plt.xlabel(), plt.ylabel(): Set the x-axis and y-axis labels.
+# plt.title(): Set the title of the plot.
+# plt.legend(): Add a legend to the plot.
+# plt.grid(): Add a grid to the plot.
+#
+# Subplots:
+# plt.subplot(): Create subplots.
+# plt.subplots(): Create multiple subplots.
+# plt.figure(): Create a figure for the plot.
+#
+# Text and Annotations:
+# plt.text(): Add text to the plot.
+# plt.annotate(): Annotate specific points on the plot.
+
+
+#
+#np concept from internet
+# x = np.array([1, 2, 3, 4])
+# y1 = np.array([10, 20, 25, 30])
+# y2 = np.array([40, 10, 30, 20])
+# plt.figure(figsize=(6, 4))
+# plt.plot(x, y1, color='red', label='Line 1')
+# plt.plot(x, y2, color='blue', label='Line 2')
+# plt.xlabel('X Values')
+# plt.ylabel('Y Values')
+# plt.title('Line Plot with Legend and Grid')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
+# explain:(6, 8) means the 6 inches wide and 8 inches tall.
+x = np.array([1, 2, 3, 4])
+y = np.array([10, 20, 25, 30])
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 8))
+# ax1=plt.subplot(121)
+ax1=plt.subplot(211)
+ax1.plot(x, y, color='blue')
+ax1.set_title('line graph')
+ax1.set_xlabel('X-axis')
+ax1.set_ylabel('Y-axis')
+ax1.grid(True)
+# ax2=plt.subplot(122)
+ax2=plt.subplot(212)
+# for scatter
+# =================================================
+ax2.scatter(x, y, color='purple', s=100, marker='x')
+ax2.set_title('Scatter Plot')
+ax2.set_xlabel('X-axis')
+ax2.set_ylabel('Y-axis')
+ax2.grid(True)
+plt.tight_layout()
 plt.show()
+
+# x = np.array([1, 2, 3, 4])
+# y = np.array([10, 20, 25, 30])
+# plt.figure(figsize=(6, 4))
+# plt.scatter(x, y, color='purple', s=100, marker='o')
+# plt.text(4, 30, 'Max Value', fontsize=12, color='black')
+# plt.annotate('Peak', xy=(3, 25), xytext=(2, 28),
+#              arrowprops=dict(facecolor='black', shrink=0.05))
+# plt.title('Scatter Plot with Text and Annotation')
+# plt.xlabel('X-axis')
+# plt.ylabel('Y-axis')
+# plt.grid(True)
+# plt.show()
