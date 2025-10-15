1dc354b3055768af7a81a1af3336f659e3d4afd7
sanketpatelcoding <androsanket@gmail.com>
2025-10-15 09:02:23 +0530

    phone visulisation2

--- panda-learn/phonevisulisation.py
+++ panda-learn/phonevisulisation.py
@@ -17,12 +17,23 @@ import matplotlib.pyplot as plt
 # -------------------------------------------------
 # Network Usage: Create a pie chart showing the proportion of
 # usage by each network
-import pandas as pd
-import matplotlib.pyplot as plt
-
+# df = pd.read_csv("phone_data.csv")
+# counts = df.groupby('network').size()
+#
+# plt.pie(counts.values, labels=counts.index, colors=['red', 'green', 'blue', 'yellow', 'pink', 'maroon', 'brown', 'grey', 'teal'])
+# plt.title('Proportion of use by network')
+# plt.show()
+# ------------------------------------------------------------------
+# Daily Activity: Plot a line chart showing the number of communications
+# per day
+# here to_datetime(df['date']).dt.date to grab date without time
 df = pd.read_csv("phone_data.csv")
-counts = df.groupby('network').size()
+df['date_only'] = pd.to_datetime(df['date']).dt.date
+counts = df.groupby('date_only').size()
 
-plt.pie(counts.values, labels=counts.index, colors=['red', 'green', 'blue', 'yellow', 'pink', 'maroon', 'brown', 'grey', 'teal'])
-plt.title('Proportion of use by network')
+plt.plot(counts.index, counts.values, color='#2196F3')
+plt.xlabel('Date')
+plt.ylabel('Number of Communications')
+plt.title('Daily Communication Activity')
+plt.xticks(rotation=45)
 plt.show()
